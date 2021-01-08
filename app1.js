c00ef634be9cb0beb37e24e5c6af6b768937e886
Rameez Yousefi <rameezy@gmail.com>
2021-01-07 23:40:06 -0800

    added another javascript function

--- app1.js
+++ app1.js
@@ -16,3 +16,28 @@ function askUserName() {
     }
   }
   
+  function printRatings() {
+    var src = '<img src="/star.png">';
+    var messedUp = "You messed up, try again!";
+    var bad = true;
+  
+    while (bad){
+      var starRating = prompt("Please give a rating 1-5");
+  
+      if (starRating < 1) {
+        document.write(messedUp);
+      }
+      else if (starRating > 5) {
+        document.write(messedUp);
+      }
+      else {
+        bad = false;
+      } 
+    }
+  
+    for (var i = 0; i < starRating; i++)
+    {
+      document.write(src);
+    }
+    
+  }
--- practice.js
+++ practice.js
@@ -1,17 +1,25 @@
-function askUserName() {
-  var userName = prompt("Please Enter Your Name");
+function printRatings() {
+  var src = '<img src="/star.png">';
+  var messedUp = "You messed up, try again!";
+  var bad = true;
 
-  if (userName == "") {
-    document.write("You messed up!");
-  } else {
-    alert("Hello " + userName);
-    document.write("Welcome to the site " + userName + "!");
-  }
-}
+  while (bad){
+    var starRating = prompt("Please give a rating 1-5");
 
-function refresh() {
-  var refreshquestion = confirm("Would you like to refresh the page?");
-  if (refreshquestion == true) {
-    window.location.reload();
+    if (starRating < 1) {
+      document.write(messedUp);
+    }
+    else if (starRating > 5) {
+      document.write(messedUp);
+    }
+    else {
+      bad = false;
+    } 
   }
+
+  for (var i = 0; i < starRating; i++)
+  {
+    document.write(src);
+  }
+  
 }
